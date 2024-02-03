b9a669ead6d06d68eafc8290fb9e3f29bc70707a
suryaelidanto <suryaelidanto@gmail.com>
2024-02-03 11:08:38 +0700

    style: beautify waitlist looks

--- app/constants/general.ts
+++ app/constants/general.ts
@@ -1,2 +1,2 @@
 export const PDF_PROJECT_PROFILE =
-  "https://drive.google.com/file/d/1w2NaPGWjA_h_53T3NFKpyNoYI5e9WeV3/view";
+  "https://drive.google.com/file/d/1GzQ7ZG9qoEAla44DMH590HG9vuoc3BL6/view";
