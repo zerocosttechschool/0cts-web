91f60db4ca5af09026a423eddc24245bfc70dc09
xirf <itsandka@gmail.com>
2024-02-12 00:25:03 +0700

    feat: contributor components

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
