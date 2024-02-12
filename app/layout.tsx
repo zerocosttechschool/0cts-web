1d52e382c95f5cfc823670b34944629bc0cc7854
Taufik-H <taufikrpl1@gmail.com>
2024-02-12 12:53:37 +0700

    add: contributor to preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import Kontributor from "./Kontributor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <main>{children}</main>
+        <Kontributor />
         <Toaster />
       </body>
     </html>
