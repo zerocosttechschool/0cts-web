ea623d1c5633f8ded22239ea8633c1944f9616c8
Taufik-H <taufikrpl1@gmail.com>
2024-02-12 12:51:47 +0700

    add: shorting class tailwind css

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  trailingComma: 'all',
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+};
